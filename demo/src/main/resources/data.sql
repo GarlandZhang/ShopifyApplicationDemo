2c50f474855b6f8cdfbd975264a15ce2adb9ee4a
GarlandZhang <garland._zhang@hotmail.com>
2018-09-24 00:03:29 -0400

    updated static data to make it more logically smoothe

--- demo/src/main/resources/data.sql
+++ demo/src/main/resources/data.sql
@@ -1,7 +1,5 @@
-INSERT INTO "User" (UserId, Username, Role) VALUES (111, 'admin111', 'ADMIN');
-INSERT INTO "User" (UserId, Username, Role) VALUES (234, 'vendor234', 'GENERAL');
-INSERT INTO "User" (UserId, Username, Role) VALUES (123, 'vendor123', 'GENERAL');
-INSERT INTO "User" (UserId, Username, Role) VALUES (100, 'customer100', 'GENERAL');
+INSERT INTO "User" (UserId, Username, Role) VALUES (234, 'user234', 'GENERAL');
+INSERT INTO "User" (UserId, Username, Role) VALUES (123, 'user123', 'GENERAL');
 INSERT INTO SHOP (ShopId, Name, Description, UserId) VALUES (1, 'House of Cards', 'Unique decks of cards for sale!', 234);
 INSERT INTO SHOP (ShopId, Name, Description, UserId) VALUES (2, 'Banana Utopia', 'Bananas from all around the world!', 123);
 INSERT INTO "Order" (OrderId, ShopId, UserId, CreationDate, UpdateDate, Total, Status) VALUES (1, 1, 234, current_date, current_date, 17.60, 'INCOMPLETE');
